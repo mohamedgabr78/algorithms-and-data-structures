1b064f5341c6e2221c4d7267a3634ce1235dfdee
Gabr <mo.gabr1994@gmail.com>
2020-11-28 02:09:59 +0200

    first algo commit

---
+++ algorithms/linearSearch.js
@@ -0,0 +1,15 @@
+// linear search
+// if we have an array of numbers
+// searching will take n of times
+// The worst case complexity is O(n), sometimes known an O(n) search.
+
+var arr = [1, 2, 3, 4, 5, 6];
+var NofTimes = 0;
+var searchNember = 6;
+for (i = 0; i < arr.length; i++) NofTimes++;
+console.log(NofTimes);
+if ((i = searchNember)) console.log("done");
+else console.log("not found");
+
+// here NofTimes will equal=6
+// and time complexity is big O(n)
